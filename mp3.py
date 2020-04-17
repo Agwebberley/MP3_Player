6cd287dd6823bcfbb38ced9c8907783d0cfaf54a
Agwebberley <44350096+Agwebberley@users.noreply.github.com>
2020-04-17 16:27:36 -0700

    Update mp3.py

--- mp3.py
+++ mp3.py
@@ -4,21 +4,35 @@ root = Tk()
 root.title("MP3")
 
 def play():
-	mixer.init()
+	print("playing")
+
 	mixer.music.load(directorye.get())
-	mixer.music.play(start=lastpos)
+	mixer.music.play()
 
-
+def unpause():
+	print("testing if unpause is needed")
+	try:
+		print("still testing")
+		if unpause == True:
+			print("unpausing")
+			print(lastpos)
+			mixer.music.unpause()
+	except:
+		print("unpause not needed")
+		play()
 
 
 def pause():
+	global unpause
+	global lastpos
+
 	lastpos = mixer.music.get_pos()
+	unpause = True
 	mixer.music.pause()
 
 
-global lastpos
+mixer.init()
 
-lastpos = 0
 
 directoryl = Label(text="Enter the Directory of the MP3")
 directoryl.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
@@ -26,7 +40,7 @@ directoryl.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 directorye = Entry(root, )
 directorye.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
 
-playB = Button(root, text="Play", command=play)
+playB = Button(root, text="Play", command=unpause)
 playB.grid(row=2, column=0)
 pauseB = Button(root, text="Pause", command=pause)
 pauseB.grid(row=2, column=1)
